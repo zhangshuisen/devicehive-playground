22c4e05eeafd5cfb9fe7775083f56473ed269704
zhangshuisen <zhangshuisen@allwinnertech.com>
2015-10-14 14:36:43 +0800

    config/api.js

--- config/api.js
+++ config/api.js
@@ -7,11 +7,11 @@
 
 module.exports = {
 
-    api_url: envexpand(process.env.DH_API_URL) || "http://playground.devicehive.com/api/rest",
+    api_url: envexpand(process.env.DH_API_URL) || "http://121.42.141.113/api/rest",
     admin_url: envexpand(process.env.DH_ADMIN_URL) || "http://playground.devicehive.com/admin",
-    swagger_url: envexpand(process.env.DH_SWAGGER_URL) || "http://playground.devicehive.com/api/swagger",
+    swagger_url: envexpand(process.env.DH_SWAGGER_URL) || "http://121.42.141.113/api/swagger",
 
-    api: envexpand(process.env.DH_API) || "http://playground.devicehive.com/api/rest",
+    api: envexpand(process.env.DH_API) || "http://121.42.141.113/api/rest",
     api_key: process.env.DH_API_KEY || 'DFXFrZ8VQIkOYECScBbBwsYinj+o8IlaLsRQ81wO+l8',
 
     secret: process.env.DH_SECRET || 'in god we trust'
